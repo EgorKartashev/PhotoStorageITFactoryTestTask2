fe1fccbee412fa02f3829b3ade0c0dd8784c88a8
Egor Kartashev <egorkarta982@gmail.com>
2023-08-28 15:08:20 +0300

    1/2 Favorite

--- PhotoStorageITFactory/VIewModel/PhotoDetailViewModel.swift
+++ PhotoStorageITFactory/VIewModel/PhotoDetailViewModel.swift
@@ -2,8 +2,18 @@
 import Foundation
 import UIKit
 
-final class PhotoDetailViewModel {
+protocol PhotoDetailViewModelProtocol {
+    var photo: Photo { get }
+    func loadImage(completion: @escaping (UIImage?) -> Void)
+}
+
+protocol PhotoDetailViewModelDelegate: AnyObject {
+    func photoDetailViewModelDidUpdateFavoriteState(viewModel: PhotoDetailViewModel)
+}
+
+final class PhotoDetailViewModel: PhotoDetailViewModelProtocol {
     var photo: Photo
+    weak var delegate: PhotoDetailViewModelDelegate?
     
     init(photo: Photo) {
         self.photo = photo
@@ -14,4 +24,22 @@ final class PhotoDetailViewModel {
             completion(image)
         }
     }
+    
+    func toggleFavorite() {
+        photo.isFavorite.toggle()
+        if photo.isFavorite {
+            var favoritePhotoIDs = UserDefaults.standard.stringArray(forKey: "favoritePhotoIDs") ?? []
+            favoritePhotoIDs.append(String(photo.id))
+            UserDefaults.standard.set(favoritePhotoIDs, forKey: "favoritePhotoIDs")
+        } else {
+            var favoritePhotoIDs = UserDefaults.standard.stringArray(forKey: "favoritePhotoIDs") ?? []
+            if let index = favoritePhotoIDs.firstIndex(of: String(photo.id)) {
+                favoritePhotoIDs.remove(at: index)
+                UserDefaults.standard.set(favoritePhotoIDs, forKey: "favoritePhotoIDs")
+            }
+        }
+        delegate?.photoDetailViewModelDidUpdateFavoriteState(viewModel: self)
+    }
+    
+    
 }
